4bc19dca3d87a0f12aec829c72d1cfd513620787
jefferyrosario <jeffrosario92@gmail.com>
2024-09-29 22:20:53 -0400

    Update 04-06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    
+    // Years that are divisible by 4
+    if(year % 4 == 0){
+
+        if( year % 400 == 0){
+            return true
+        }
+        if(year % 100 == 0){
+            return false
+        }
+        
+        return true
+    } else {
+        return false
+    }
+    // Unless the year is divisble by 100
+
+    // unless they are divisble by 400
+
 
 };
 
